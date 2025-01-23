da0604fa5786cf0414f46bada0d913dc0630ddfd
Bhavin Sakaria <bhavinsakaria01@gmail.com>
2025-01-24 00:46:58 +0530

    Dis

--- src/app/dispatch/[id]/page.tsx
+++ src/app/dispatch/[id]/page.tsx
@@ -2,8 +2,11 @@ import db from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 
-
-export default async function DispatchRecord({ params }: { params: { id: string } }) {       
+export default async function DispatchRecord({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = params; // Extract 'id' after 'params' is resolved
   const dispatchRec = await db.dispatch.findUnique({
     where: { id },
@@ -14,24 +17,53 @@ export default async function DispatchRecord({ params }: { params: { id: string 
   }
 
   return (
-    <div>
-      
-      <div>
-      <Link href="/dispatch"><button className="btn btn-base "> Back </button></Link>
+    <>
+      <div className="container mx-auto  pd-2">
+        <Link href="/dispatch">
+          <button className="btn btn-base "> Back </button>
+        </Link>
       </div>
-      
-      <div className="card w-full shadow-xl mt-2">
-        <div className="card-body">
-          <h2 className="card-title">Dispatch Record</h2>
-          <p>
-            <span className="font-bold text-xl">Challan No:</span> <span className="text-xl">{dispatchRec.challanNo}</span>
-          </p>
-          <p>
-            <span className="font-bold">Party Name:</span> {dispatchRec.partyName}{dispatchRec.partyPlace}
-          </p>
 
+      <div className="container mx-auto border pd-2 mt-2">
+        <div className=" flex justify-center mt-3 ">
+          <ul className="steps gap-4">
+            <li className="step step-success " data-content="✓">
+              Printed
+            </li>
+            <li className="step step-success" data-content="✓">
+              Packed
+            </li>
+            <li className="step" data-content="">
+              Dispatched
+            </li>
+            <li className="step" data-content="">
+              Delivered
+            </li>
+          </ul>
         </div>
-      </div>  
-    </div>
+        <div className="divider "></div>
+        <div className="pl-4">
+          <h1 className="text-xl font-bold">{dispatchRec?.partyName}</h1>
+          <p>Challan No: {dispatchRec?.challanNo}</p>
+          <p>
+            Challan Date:{" "}
+            {dispatchRec?.challanDate
+              ? new Date(dispatchRec.challanDate).toLocaleDateString()
+              : ""}
+          </p>
+          <div className="divider"></div>
+          <h1 className="text-xl font-bold">Transport Details</h1>
+          <p>Transport Name: {dispatchRec?.transportName}</p>
+          <p>LR No: {dispatchRec?.lrNo}</p>
+          <p>
+            LR Date:{" "}
+            {dispatchRec?.lrDate
+              ? new Date(dispatchRec.lrDate).toLocaleDateString()
+              : ""}
+          </p>
+          <p>Number of Cases: {dispatchRec?.numberofCases}</p>
+        </div>
+      </div>
+    </>
   );
 }
